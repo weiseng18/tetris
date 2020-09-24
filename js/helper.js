283f4cd6b3d20539d96f4f63a6687476949d3edb
weiseng18 <tws.hci@gmail.com>
2020-09-24 17:30:03 +0800

    [feature prep] added helper function getCell() to get reference to a certain cell in a table

--- js/helper.js
+++ js/helper.js
@@ -1,3 +1,9 @@
 function get(id) {
 	return document.getElementById(id);
 }
+
+// returns the HTML element of the xth tr of the yth td of a table.
+// expects id to be the id of a table.
+function getCell(id, x, y) {
+	return document.getElementById(id).children[0].children[x].children[y];
+}
